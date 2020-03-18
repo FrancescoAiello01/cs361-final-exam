759356995d76959b113ac5abfa1b7d5dd5464969
Francesco Aiello <aiellof@oregonstate.edu>
2020-03-18 11:34:17 -0700

    abstract_storage replaces old storage classes

--- roll_pack.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'abstract_storage'
-
-class RollPack < AbstractStorage
-
-  def initialize
-    @price = 1
-  end
-
-end
--- tail_pack.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'abstract_storage'
-
-class TailPack < AbstractStorage
-
-  def initialize
-    @cost = 2
-  end
-
-end
