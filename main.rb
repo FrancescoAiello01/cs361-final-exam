b5a441c9adf38745094c111e24a15f2aee9cab65
Francesco Aiello <aiellof@oregonstate.edu>
2020-03-18 11:37:32 -0700

    reformat main.rb

--- main.rb
+++ main.rb
@@ -1,17 +1,12 @@
-# As a customer,
-# I want to estimate the cost of renting four bikes
-#   for a week
-# So that I know how much money I will need to pay
-
 require_relative 'bmx_bike'
 require_relative 'road_bike'
 require_relative 'mountain_bike'
 
 bikes = [
-  BmxBike.new,
-  RoadBike.new,
-  MountainBike.new,
-  MountainBike.new
+    BmxBike.new,
+    RoadBike.new,
+    MountainBike.new,
+    MountainBike.new
 ]
 
 total_price = 0
